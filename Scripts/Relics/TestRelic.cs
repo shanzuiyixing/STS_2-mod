4416b0f6e98ca389abd44e1a5c1fdcac0a94864b
Justin Bieber <114400466+shanzuiyixing@users.noreply.github.com>
2026-04-04 19:06:24 +0800

    初始化卡牌和遗物 Initial commit

---
+++ Scripts/Relics/TestRelic.cs
@@ -0,0 +1,34 @@
+// 加入哪个遗物池，此处为通用
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.Entities.Players;
+
+namespace Test.Scripts.Relics;
+
+[Pool(typeof(SharedRelicPool))]
+public class TestRelic : CustomRelicModel
+{
+    // 稀有度
+    public override RelicRarity Rarity => RelicRarity.Common;
+
+    // 遗物的数值。替换本地化中的{Cards}。
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(1)];
+
+    // 小图标
+    public override string PackedIconPath => $"res://Test/images/relics/{Id.Entry.ToLowerInvariant()}.png";
+    // 轮廓图标
+    protected override string PackedIconOutlinePath => $"res://Test/images/relics/{Id.Entry.ToLowerInvariant()}.png";
+    // 大图标
+    protected override string BigIconPath => $"res://Test/images/relics/{Id.Entry.ToLowerInvariant()}.png";
+
+    public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
+    {
+        // 这里的DynamicVars.Cards.IntValue为上面设置的CardsVar的数值。
+        await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.IntValue, player);
+    }
+}
