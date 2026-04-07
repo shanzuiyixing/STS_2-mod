b38c213014adfe352b06d449e5a484ae052da9e2
Justin Bieber <114400466+shanzuiyixing@users.noreply.github.com>
2026-04-07 23:13:08 +0800

    增加联机卡模板

--- Scripts/Relics/PlaytodrawRelic.cs
+++ Scripts/Relics/PlaytodrawRelic.cs
@@ -2,20 +2,21 @@ using BaseLib.Abstracts;
 using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.RelicPools;
-using Test.Scripts.Powers;
-
+using MegaCrit.Sts2.Core.Entities.Players;
 
 namespace Test.Scripts.Relics;
 
 [Pool(typeof(SilentRelicPool))]
-public class LifeswapRelic : CustomRelicModel
+public class PlaytodrawRelic : CustomRelicModel
 {
     // 稀有度
     public override RelicRarity Rarity => RelicRarity.Common;
 
     // 遗物的数值。替换本地化中的{Cards}。
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(1)];
 
     // 小图标
     public override string PackedIconPath => $"res://Test/images/relics/{Id.Entry.ToLowerInvariant()}.png";
@@ -24,13 +25,9 @@ public class LifeswapRelic : CustomRelicModel
     // 大图标
     protected override string BigIconPath => $"res://Test/images/relics/{Id.Entry.ToLowerInvariant()}.png";
 
-    public override bool HasUponPickupEffect => true;
-
-    public override async Task BeforeCombatStart()
-	{
-		Flash();
-        await PowerCmd.Apply<LifeswapPower>(base.Owner.Creature,base.DynamicVars["LifeswapPower"].BaseValue,base.Owner.Creature,null);
-	}
-
-
+    public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
+    {
+        // 这里的DynamicVars.Cards.IntValue为上面设置的CardsVar的数值。
+        await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.IntValue, player);
+    }
 }
